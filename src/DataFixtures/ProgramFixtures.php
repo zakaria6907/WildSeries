9dfe358e086b01bbff092bbe8a2e24a04391196a
zakaria6907 <bourmelzakaria@outlook.fr>
2023-05-24 14:30:12 +0200

    09 - Symfony : Les fixtures avancées avec jointures

---
+++ src/DataFixtures/ProgramFixtures.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Program;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ProgramFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $programs = [
+            [
+                'title' => 'Walking Dead',
+                'synopsis' => 'Des zombies envahissent la terre',
+                'poster' => 'zack',
+                'category' => $this->getReference('category_Action'),
+            ],
+            [
+                'title' => 'Game of Thrones',
+                'synopsis' => 'Intrigues et batailles pour le Trône de Fer',
+                'poster' => 'got',
+                'category' => $this->getReference('category_Aventure'),
+            ],
+            [
+                'title' => 'Friends',
+                'synopsis' => 'Les aventures de six amis à New York',
+                'poster' => 'friends',
+                'category' => $this->getReference('category_Animation'),
+            ],
+            [
+                'title' => 'Breaking Bad',
+                'synopsis' => 'Un professeur de chimie devient un puissant trafiquant de drogue',
+                'poster' => 'breaking_bad',
+                'category' => $this->getReference('category_Fantastique'),
+            ],
+            [
+                'title' => 'Stranger Things',
+                'synopsis' => 'Des événements étranges se produisent dans une petite ville',
+                'poster' => 'stranger_things',
+                'category' => $this->getReference('category_Horreur'),
+            ],
+        ];
+
+        foreach ($programs as $data) {
+            $program = new Program;
+            $program->setTitle($data['title']);
+            $program->setSynopsis($data['synopsis']);
+            $program->setPoster($data['poster']);
+            $program->setCategory($data['category']);
+            $manager->persist($program);
+        }
+
+        $manager->flush();
+    }
+    public function getDependencies()
+    {
+        // Tu retournes ici toutes les classes de fixtures dont ProgramFixtures dépend
+        return [
+          CategoryFixtures::class,
+        ];
+    }
+}
