7db56327eafe219a9ab73c6e24e3a0a5b7050d0e
zakaria6907 <bourmelzakaria@outlook.fr>
2023-05-24 11:19:39 +0200

    add entity category and program

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,20 +1,36 @@
 <?php
-// src/Controller/ProgramController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
-    public function index(): Response
-    {
-        return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
-         ]);
      
+    #[Route('/{id}/', name: 'show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function show(int $id): Response
+    {
+        // Affichage de l'id du programme dans une balise <h1>
+        return $this->render('program/list.html.twig', [
+            'id' => $id,
+        ]);
     }
+
+     #[Route('/', name: 'index')]
+     public function index(): Response
+     {
+         // ...
+         return $this->render('program/list.html.twig');
+     }
+
+     #[Route('/new', name: 'new')]
+     public function new(): Response
+     {
+         // ...
+         return $this->redirectToRoute('program_show', ['id' => 4]);
+     }
+
+
 }
